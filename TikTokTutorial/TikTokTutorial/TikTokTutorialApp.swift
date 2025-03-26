bf807025f3921f4bf7bf2d08959dea539c0b4506
dxd <2459674360@qq.com>
2025-03-26 10:49:25 +0800

    feat:初始化tiktok tutorial项目

---
+++ TikTokTutorial/TikTokTutorial/TikTokTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokTutorialApp.swift
+//  TikTokTutorial
+//
+//  Created by NEP on 2025/3/26.
+//
+
+import SwiftUI
+
+@main
+struct TikTokTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
