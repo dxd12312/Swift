16cb440d25bebc5caa0ea290e19341c701222fd0
dxd <2459674360@qq.com>
2025-03-28 08:44:47 +0800

    feat:初建项目结构以及进行首页搭建

---
+++ TikTokTutorial/TikTokTutorial/Core/Feed/FeedView.swift
@@ -0,0 +1,22 @@
+//
+//  FeedView.swift
+//  TikTokTutorial
+//
+//  Created by NEP on 2025/3/26.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    FeedView()
+}
